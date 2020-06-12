e7f36f483f672fcd6abf07f0ce9361012dc51687
khairulizwan <wansnap@gmail.com>
2020-06-12 10:50:31 +0800

    Updated on 06-12-2020-09:19:51 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/raspicam_haar_face_detection.py
+++ script/raspicam_haar_face_detection.py
@@ -73,7 +73,7 @@ class HaarFaceDetector:
 		self.detectHaarFace()
 
 		# Show an Image
-		self.showImage()
+#		self.showImage()
 
 		self.pubRegionofInterest()
 
@@ -149,9 +149,9 @@ class HaarFaceDetector:
 		img_title = self.timestr + "-photo.jpg"
 		if self.face_detected:
 			cv2.imwrite(img_title, self.frameClone)
-			rospy.logwarn("Face Detect")
+#			rospy.logwarn("Face Detect")
 		else:
-			rospy.logwarn("No Face Detect")
+#			rospy.logwarn("No Face Detect")
 			pass
 
 def main(args):
